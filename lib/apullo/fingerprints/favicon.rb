8be06ccb730114878da1a3890f9c9fa261f56173
Manabu Niseki <manabu.niseki@gmail.com>
2019-11-05 08:53:34 +0900

    feat: improve favicon detection

--- lib/apullo/fingerprints/favicon.rb
+++ lib/apullo/fingerprints/favicon.rb
@@ -34,7 +34,7 @@ module Apullo
       def b64_favicon_data
         @b64_favicon_data ||= [].tap do |out|
           data = get(uri.path)
-          break unless data
+          next unless data
 
           b64 = Base64.strict_encode64(data)
           out << b64.chars.each_slice(76).map(&:join).join("\n") + "\n"
@@ -46,7 +46,8 @@ module Apullo
         path = path.empty? ? "/" : path
         request = Net::HTTP::Get.new(path)
         response = http.request(request)
-        response.body
+
+        response.code.to_i == 200 ? response.body : nil
       rescue Errno::ECONNREFUSED, Net::HTTPError, OpenSSL::OpenSSLError => _e
         nil
       end
